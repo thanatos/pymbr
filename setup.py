4ca37859dd001050b5685eddc7dfe96803204444
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2013-12-18 21:54:57 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pymbr',
+    version='1.0',
+    description='MBR (master boot record) library',
+    author='Roy Wellington',
+    url='https://github.com/thanatos/pymbr',
+    py_modules=['mbr'],
+)
